28141a46a54965d8859a33441f3bb32f22c89a83
Alain Keller <alain.keller@ost.ch>
2023-12-27 15:55:58 +0100

    plotly dash is trash

--- dronetracker/ui/liveBeamPlot.py
+++ dronetracker/ui/liveBeamPlot.py
@@ -5,7 +5,11 @@ import dash
 from dash import Dash, dcc, html, Input, Output, callback
 import plotly
 import plotly.graph_objects as go
+import plotly.express as px
 from plotly.subplots import make_subplots
+
+from scipy.interpolate import griddata
+import matplotlib.pyplot as plt
 
 from beamforming.prototypeTracker import Tracker
 class UI():
@@ -29,6 +33,7 @@ class UI():
         self.zz = cos(self.theta)
         self.x_hat = []
         self.y_hat = []
+        self.im = np.random.random_integers(low=0, high=255, size=(100,100,3))
 
 
         external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -72,32 +77,47 @@ class UI():
             y = self.yy
             z = self.zz
             fig = make_subplots(
-            rows=1, cols=3,
-            specs=[[{'type': 'mesh3d'}, {'type': 'mesh3d'}, {'type':'scatter'}]])
+            rows=2, cols=2,
+            specs=[[{'type': 'mesh3d'}, {'type': 'mesh3d'}],
+                    [{'type':'scatter'}, {'type': 'image'}]]
+            )
 #             fig = 
 #             fig = go.Figure(data=[go.Mesh3d(z=(response), x=(self.x), y=(self.y),
+            '''
             fig.add_trace(go.Mesh3d(z=(response), x=(self.x), y=(self.y),
                                 intensity=response, colorscale='Viridis'),
                             row=1,col=1)
             fig.add_trace(go.Mesh3d(z=z, x=x, y=y,
                                     intensity=r, colorscale='Viridis'),
                             row=1,col=2)
-
-            fig.add_trace(go.Scatter(
+            '''
+            '''fig.add_trace(go.Scatter(
                             x=self.x_hat,
                             y=self.y_hat,
                             mode="lines+markers",
                             name="Data",
                             line={"color": "rgb(0, 255, 0)"},
                             marker={"color": "rgb(0, 255, 0)", "size": 8},
-#                             layout_yaxis_range=[-4,4],
-                            ), row=1, col=3)
+                            ), row=2, col=1)
+            '''
+            grid_x, grid_y = np.mgrid[-1.6:1.6:100j, -1.6:1.6:100j]
+            grid = griddata((self.x,self.y), response, (grid_x, grid_y), method='nearest').T
+            grid = np.repeat(grid[:, :, np.newaxis], 1, axis=2)
+            grid = grid - np.min(grid)
+            grid = grid/np.max(grid) * 255
+            gird = grid.astype(np.uint8).copy()
+            print(grid.shape)
+            print(self.im.shape)
+#             fig.add_trace(px.imshow(grid).data[0], row=2, col=2)
+#             fig.add_trace(go.Image(z=grid), row=2, col=2)
+            fig.add_trace(go.Image(z=self.im), row=2, col=2)
 #             fig = go.Figure(data=[go.Mesh3d(z=(z), x=(x), y=(y),
 #                             intensity=r, colorscale='Viridis')],
 #                             layout=self.layout).set_subplots(1,1)
-            fig.update_layout(width=1800, height=900, uirevision=1)
-            fig.update_yaxes(range=[-1.7, 1.7], scaleanchor="x", scaleratio=1,row=1, col=3)
-            fig.update_xaxes(range=[-1.7, 1.7], row=1, col=3)
+            fig.update_layout(width=1600, height=1400, uirevision=1)
+            fig.update_yaxes(range=[-1.7, 1.7], scaleanchor="x", scaleratio=1,row=2, col=1)
+            fig.update_xaxes(range=[-1.7, 1.7], row=2, col=1)
+
 #             fig.update_layout(width=1500, height=900)
 #             fig2 = go.Figure(data=[go.Mesh3d(z=(response), x=(self.x), y=(self.y),
 #                             intensity=r, colorscale='Viridis')],
